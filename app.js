24321ac804ade233cbf8ad3ed78377631f6aa3b0
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-03-26 19:29:14 +0200

    mise a jour

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 const bouton = document.querySelector('.impression');
+const href = document.querySelectorAll('a');
+const white = document.querySelectorAll('.white');
+const competences = document.querySelector('.compétences-list');
+const sections = document.querySelectorAll('section')
 function impress() {
-    bouton.addEventListener('click' ,() => {
-        window.print();
-    })
-}
+    bouton.addEventListener('click', () => {
+      white.forEach(whites => {
+        whites.style.color = 'black';
+      });
+  
+      href.forEach(hrefs => {
+        hrefs.style.color =  'black';
+      });
+  
+      competences.style.display = 'grid';
+      competences.style.gridTemplateColumns = 'repeat(2, 1fr)';
+      competences.style.gridTemplateRows = 'repeat(2, 1fr)';
+      competences.style.gridTemplateAreas = 
+        "primo deuxio"
+        "tertio quartio";
+
+      sections.forEach(section => {
+        section.style.padding ='20em 4em 0 4em';
+      }) 
+  
+      window.print();
+    });
+  }
+  
 impress();
+
+
+
+window.onafterprint = function() {
+    white.forEach(whites => {
+        whites.style.color = 'white';
+      });
+    href.forEach(hrefs => {
+          hrefs.style.color =  'white';
+        });
+    competences.style.display = ''; 
+    
+    sections.forEach(section => {
+      section.style.padding ='';
+    }) 
+  };
